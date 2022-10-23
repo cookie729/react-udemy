33fc973634ee28a47e6a5fd51b21ca4ef344fcb8
sakai kouki <cookie.@sakainoMacBook-Air.local>
2022-10-23 22:00:34 +0900

    useEffect

--- react-redux-hooks/src/App.js
+++ react-redux-hooks/src/App.js
@@ -1,22 +1,25 @@
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App = (props) => {
-  // propsをそのまま渡すことができる
   const [state, setState] = useState(props);
-  // const [name, setName] = useState(props.name);
-  // const [price, setPrice] = useState(props.price);
   const { name, price } = state;
 
-  // const reset =() => {
-  //   setPrice(props.price)
-  //   setName(props.name)
-  // }
+  useEffect(() => {
+    console.log('This is like component');
+  });
+  useEffect(() => {
+    console.log('This is like component rrrrrrrrrr');
+  },[]);
+  useEffect(() => {
+    console.log('This callback is for name only');
+  },[name]);
+
 
   return (
     <>
       <p>
-        現在の{name}は、{price}円です
+        現在の{name}は、{price}円です。
       </p>
       <button onClick={() => setState({ ...state, price: price + 1 })}>
         +1
@@ -24,15 +27,11 @@ const App = (props) => {
       <button onClick={() => setState({ ...state, price: price - 1 })}>
         -1
       </button>
-      {/* <button onClick={() => setPrice(price + 1)}>+1</button> */}
-      {/* <button onClick={() => setPrice(price - 1)}>-1</button> */}
       <button onClick={() => setState(props)}>Reset</button>
-      {/* <button onClick={reset}>Reset</button> */}
       <input
         value={name}
         onChange={(e) => setState({ ...state, name: e.target.value })}
       />
-      {/* <input value={name} onChange={(e) => setName(e.target.value)} /> */}
     </>
   );
 };
